d07b3177a578b6f6e04908284de8dc805ca0dce8
pupyk <pupykint@gmail.com>
2025-09-15 22:39:05 +0000

    First try to do hw

---
+++ hw_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int bin_count(int a){
+    int count = 0;
+    do{
+        count += a % 2 == 1? 1 : 0;
+        a /= 2;
+    }
+    while (a > 0);
+    return count;
+}
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    int result;    
+    for (int i = a; i <= b; i++){
+        result += bin_count(i);    
+    }
+    cout << "Result is " << result << "\n";
+}
