13c7e4d74682ee1c9b4e69129ce9f623a22390aa
pupyk <pupykint@gmail.com>
2025-09-18 15:21:33 +0000

    Change architecture a bit

--- src_hw_1/main.cpp
+++ src_hw_1/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+
 using namespace std;
 
 int bin_count(int a){
-    int count = 0;
+    int count {0};
     do{
-        count += a % 2 == 1? 1 : 0;
+        count += a % 2 == 1 ? 1 : 0;
         a /= 2;
     }
     while (a > 0);
@@ -14,7 +15,7 @@ int bin_count(int a){
 int main(){
     int a, b;
     cin >> a >> b;
-    int result;    
+    int result {0};    
     for (int i = a; i <= b; i++){
         result += bin_count(i);    
     }
